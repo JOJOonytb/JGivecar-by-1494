0e389e3831f08caefb8f9c10aa130c19d26cf706
JOJO <104567374+JOJOonytb@users.noreply.github.com>
2022-08-04 11:56:05 +0200

    Add files via upload

---
+++ JGivecar by JOJO/JGivecar/locales/fr.lua
@@ -0,0 +1,8 @@
+Locales['fr'] = {
+	['gived_car'] = 'Le véhicule ~y~%s ~s~avec le numéro de plaque ~y~ %s ~s~a été garé dans le garage de ~g~%s~s~\',
+	['received_car'] = 'Vous avez reçu un véhicule avec le numéro de plaque ~y~%s',
+	['del_car'] = 'A supprimé un véhicule avec le numéro de plaque ~y~%s',
+	['none_plate'] = '~r~Vous devez saisir une plaque personnalisée',
+	['unknown_car'] = '~r~Nom de voiture inconnu',
+	['plate_already_have'] = '~r~Cette plaque a déjà été utilisée sur un autre véhicule'
+	}
